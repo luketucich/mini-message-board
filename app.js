17ddb5034b1f9d598d46b2448bc1ca819fbf0332
luketucich <ltucich@gmail.com>
2025-02-11 12:10:14 -0500

    feat: Set up index router + view, and messages are now displayed at /.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/indexRouter");
 const path = require("node:path");
 
-// Enables EJS templating
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Mini Message Board - listening on port ${PORT}!`);
+  console.log(`My first Express app - listening on port ${PORT}!`);
 });
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -14,3 +14,9 @@ const messages = [
     added: new Date(),
   },
 ];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+module.exports = indexRouter;
